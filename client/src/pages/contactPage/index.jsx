b7892606aee4c025d488f46fe2cfbba54400c636
@r0f33 <jiske.nicolson@gmail.com>
2024-07-11 11:15:16 +0930

    src/pages/contact & /src/components/form page plus styling  + /src/utils/helpers contained within

--- client/src/pages/contactPage/index.jsx
+++ client/src/pages/contactPage/index.jsx
@@ -1,22 +1,17 @@
+import Form from "../../components/Form";
+import FlexStyling from "../../components/FlexStyling";
+import { useMediaQuery } from "@mui/material";
+
 export default function Contact() {
+    const isNonMobileScreens = useMediaQuery("(min-width: 1200px");
+
     return (
-        <div>
-            <h1>Contact Page</h1>
-            <p>
-                Integer cursus bibendum sem non pretium. Vestibulum in aliquet
-                sem, quis molestie urna. Aliquam semper ultrices varius. Aliquam
-                faucibus sit amet magna a ultrices. Aenean pellentesque placerat
-                lacus imperdiet efficitur. In felis nisl, luctus non ante
-                euismod, tincidunt bibendum mi. In a molestie nisl, eu sodales
-                diam. Nam tincidunt lacus quis magna posuere, eget tristique dui
-                dapibus. Maecenas fermentum elementum faucibus. Quisque nec
-                metus vestibulum, egestas massa eu, sollicitudin ipsum. Nulla
-                facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh dictum
-                ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-                conubia nostra, per inceptos himenaeos. Etiam ornare rutrum
-                felis at rhoncus. Etiam vel condimentum magna, quis tempor
-                nulla.
-            </p>
-        </div>
+        <FlexStyling
+            margin="5%"
+            borderRadius="0.2rem"
+            style={isNonMobileScreens ? { width: "45%" } : { width: "auto" }}
+        >
+            <Form />
+        </FlexStyling>
     );
 }
